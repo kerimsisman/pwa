5ee35aa08e043b78739ddeea182d82cf2147bdc0
kerimsisman <67425241+kerimsisman@users.noreply.github.com>
2022-10-17 22:33:04 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ deferredIntall:null,
 
 init(){
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(()=>{
+        navigator.serviceWorker.register('/pwa/sw.js').then(()=>{
           console.log('!!!!!!!!!!!!Service worker registered!!!');
         });
       }
